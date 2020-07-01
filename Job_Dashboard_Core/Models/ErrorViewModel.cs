1a77d4b4db593ea5b8994d01b20091e1612937c8
Alejandro Solares <josue.alejandro.solares@gmail.com>
2020-07-01 14:23:49 -0600

    Commit inicial
    Creacion del repo

---
+++ Job_Dashboard_Core/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Job_Dashboard_Core.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Job_Dashboard_Core/Models/Jobs_crud.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Job_Dashboard_Core.Models
+{
+    public class Jobs_crud
+    {
+    }
+}
