4606477aee93197058890b2a7cb372ac74b28efe
Alejandro Solares <josue.alejandro.solares@gmail.com>
2020-07-01 19:00:54 -0600

    Middle_Data

---
+++ NetCore-CRUD/Jobs_Dashboard_core/MiddleData/Jobs_Context.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Jobs_Dashboard_core.Models;
+
+namespace Jobs_Dashboard_core.MiddleData
+{
+    public class Jobs_Context : DbContext
+    {
+        public Jobs_Context(DbContextOptions<Jobs_Context> options) : base(options)
+        { }
+
+        //Tables
+        public DbSet<Jobs> Jobs { get; set; }
+        public DbSet<Job_Requestor> Job_Requestors { get; set; }
+        public DbSet<Job_Type> Job_Types { get; set; }
+
+
+    }
+}
